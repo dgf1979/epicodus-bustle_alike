ad2eb026243572548811de195e2d2fccf91a493f
Drew Finstrom & Ben Cornelis <student@epicodus.com>
2015-06-08 15:10:28 -0700

    add image carousel using ember functionality

--- controllers/news.js
+++ controllers/news.js
@@ -1,3 +1,3 @@
 App.NewsController = Ember.Controller.extend({
-  
+
 })
--- router.js
+++ router.js
@@ -1,5 +1,10 @@
 App.Router.map(function() {
-  this.resource('news', {path: '/'});
+  this.resource('news', {path: '/'}, function() {
+    this.resource('starbuckscarousel', {path:'/'});
+    this.resource('friescarousel', {path:'fries'});
+    this.resource('gotcarousel', {path:'game-of-thrones'});
+    this.resource('applewatchcarousel', {path:'apple-watch'});
+  });
   this.resource('news-item', function() {
     this.resource('fries'),
     this.resource('GOT', {path: 'game-of-thrones'}),
